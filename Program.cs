7e132970617554425260c19530ef3f4d31c024a1
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-06 11:20:06 +0530

    [sunil] implemented contact class with variables

--- Program.cs
+++ Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AddressBookSystem
+namespace Address_Book_System
 {
+    class Contacts
+    {
+        private string first_name;
+        private string last_name;
+        private string email;
+        private string phone_number;
+        private string city;
+        private string state;
+        private string zipcode;
+        private string address;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            Contacts person = new Contacts();
+            Console.WriteLine("Welcome to Address Book");
+            Console.ReadLine();
+
         }
     }
 }
